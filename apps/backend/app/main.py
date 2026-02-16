d6bfacd6bf7ac4871248531f551f659825149b98
JeanVittory <vittory.dev@gmail.com>
2026-02-15 21:26:42 -0500

    fix(backend): parse ALLOWED_ORIGINS from .env

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -10,7 +10,7 @@ def create_app() -> FastAPI:
 
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.allowed_origins,
+        allow_origins=settings.allowed_origins_list,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
